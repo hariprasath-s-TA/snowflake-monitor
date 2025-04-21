2af0d4c3ec76356c97d9c5a91f211f226a1d3547
hariprasath-s-TA <hariprasath.s@tigeranalytics.com>
2025-04-21 18:02:27 +0530

    integrated-LLM-chatbot-for-result-analysis

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ st.session_state['session'].sql("CALL AUTOMATE_DASHBOARD_VIEW()").collect()
 
 pg = st.navigation([
     st.Page("app_pages/dashboard.py", title="Dashboard", icon="🖥️"),
-    st.Page("app_pages/addMonitors.py", title="Add Monitors", icon="➕")
+    st.Page("app_pages/addMonitors.py", title="Add Monitors", icon="➕"),
+    st.Page("app_pages/resultsDashboard.py", title="Result Explore", icon="📈")
 ])
 pg.run()
